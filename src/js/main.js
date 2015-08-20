008099d5352d684785ca5881a3821f4a26ce2128
pkrater <info@krater.se>
2015-08-20 14:11:32 +0200

    update
    improvements everywhere

--- src/js/main.js
+++ src/js/main.js
@@ -25,6 +25,91 @@ var sprig = {
 
       // JavaScript to be fired on all pages
 
+      // Google Maps Scripts
+       // When the window has finished loading create our google map below
+       google.maps.event.addDomListener(window, 'load', init);
+
+       function init() {
+           // Basic options for a simple Google Map
+           // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
+           var mapOptions = {
+               // How zoomed in you want the map to start at (always required)
+               zoom: 15,
+
+               // The latitude and longitude to center the map (always required)
+               center: new google.maps.LatLng(59.27081,	18.00412 ), // Älvsjö
+
+               // Disables the default Google Maps UI components
+               disableDefaultUI: true,
+               scrollwheel: false,
+               draggable: false,
+
+               // How you would like to style the map.
+               // This is where you would paste any style found on Snazzy Maps.
+               styles: [{
+        "featureType": "all",
+        "stylers": [{
+                "saturation": 0
+            },
+            {
+                "hue": "#e7ecf0"
+            }
+        ]
+    },
+    {
+        "featureType": "road",
+        "stylers": [{
+                "saturation": -70
+            }
+        ]
+    },
+    {
+        "featureType": "transit",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "poi",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "stylers": [
+            {
+                "visibility": "simplified"
+            },
+            {
+                "saturation": -60
+            }]
+    }]
+           };
+
+           // Get the HTML DOM element that will contain your map
+           // We are using a div with id="map" seen below in the <body>
+           var mapElement = document.getElementById('map');
+
+           // Create the Google Map using out element and options defined above
+           var map = new google.maps.Map(mapElement, mapOptions);
+
+           // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
+           var image = './wp-content/themes/krater-base-theme/dist/img/map-marker2.png';
+           var myLatLng = new google.maps.LatLng(59.27081, 18.00412 );
+           var beachMarker = new google.maps.Marker({
+               position: myLatLng,
+               map: map,
+               icon: image
+           });
+       }
+
+
+
       //ResponsiveMenu
 
       $(document).ready(function()
@@ -52,161 +137,16 @@ var sprig = {
 
 
 
-        $('.navbar-default').ResponsiveMenu(200);
 
 
-        // Google Maps Scripts
-        // When the window has finished loading create our google map below
-        google.maps.event.addDomListener(window, 'load', init);
 
-        function init() {
-            // Basic options for a simple Google Map
-            // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-            var mapOptions = {
-                // How zoomed in you want the map to start at (always required)
-                zoom: 15,
-
-                // The latitude and longitude to center the map (always required)
-                center: new google.maps.LatLng(59.27081,	18.00412 ), // Älvsjö
-
-                // Disables the default Google Maps UI components
-                disableDefaultUI: true,
-                scrollwheel: false,
-                draggable: false,
-
-                // How you would like to style the map.
-                // This is where you would paste any style found on Snazzy Maps.
-                styles: [{
-                    "featureType": "water",
-                    "elementType": "geometry",
-                    "stylers": [{
-                        "color": "#000000"
-                    }, {
-                        "lightness": 17
-                    }]
-                }, {
-                    "featureType": "landscape",
-                    "elementType": "geometry",
-                    "stylers": [{
-                        "color": "#000000"
-                    }, {
-                        "lightness": 20
-                    }]
-                }, {
-                    "featureType": "road.highway",
-                    "elementType": "geometry.fill",
-                    "stylers": [{
-                        "color": "#000000"
-                    }, {
-                        "lightness": 17
-                    }]
-                }, {
-                    "featureType": "road.highway",
-                    "elementType": "geometry.stroke",
-                    "stylers": [{
-                        "color": "#000000"
-                    }, {
-                        "lightness": 29
-                    }, {
-                        "weight": 0.2
-                    }]
-                }, {
-                    "featureType": "road.arterial",
-                    "elementType": "geometry",
-                    "stylers": [{
-                        "color": "#000000"
-                    }, {
-                        "lightness": 18
-                    }]
-                }, {
-                    "featureType": "road.local",
-                    "elementType": "geometry",
-                    "stylers": [{
-                        "color": "#000000"
-                    }, {
-                        "lightness": 16
-                    }]
-                }, {
-                    "featureType": "poi",
-                    "elementType": "geometry",
-                    "stylers": [{
-                        "color": "#000000"
-                    }, {
-                        "lightness": 21
-                    }]
-                }, {
-                    "elementType": "labels.text.stroke",
-                    "stylers": [{
-                        "visibility": "on"
-                    }, {
-                        "color": "#000000"
-                    }, {
-                        "lightness": 16
-                    }]
-                }, {
-                    "elementType": "labels.text.fill",
-                    "stylers": [{
-                        "saturation": 36
-                    }, {
-                        "color": "#000000"
-                    }, {
-                        "lightness": 40
-                    }]
-                }, {
-                    "elementType": "labels.icon",
-                    "stylers": [{
-                        "visibility": "off"
-                    }]
-                }, {
-                    "featureType": "transit",
-                    "elementType": "geometry",
-                    "stylers": [{
-                        "color": "#000000"
-                    }, {
-                        "lightness": 19
-                    }]
-                }, {
-                    "featureType": "administrative",
-                    "elementType": "geometry.fill",
-                    "stylers": [{
-                        "color": "#000000"
-                    }, {
-                        "lightness": 20
-                    }]
-                }, {
-                    "featureType": "administrative",
-                    "elementType": "geometry.stroke",
-                    "stylers": [{
-                        "color": "#000000"
-                    }, {
-                        "lightness": 17
-                    }, {
-                        "weight": 1.2
-                    }]
-                }]
-            };
-
-            // Get the HTML DOM element that will contain your map
-            // We are using a div with id="map" seen below in the <body>
-            var mapElement = document.getElementById('map');
-
-            // Create the Google Map using out element and options defined above
-            var map = new google.maps.Map(mapElement, mapOptions);
-
-            // Custom Map Marker Icon - Customize the map-marker.png file to customize your icon
-            var image = './wp-content/themes/krater-base-theme/dist/img/map-marker.png';
-            var myLatLng = new google.maps.LatLng(59.27081, 18.00412 );
-            var beachMarker = new google.maps.Marker({
-                position: myLatLng,
-                map: map,
-                icon: image
-            });
-        }
 
 
 
 
       });
+
+
 
     }
   },
@@ -214,6 +154,13 @@ var sprig = {
   home: {
     init: function() {
       // JavaScript to be fired on the home page
+
+
+
+      function myscroll(myID){
+    var offset = jQuery("#"+myID).offset()
+    window.scrollTo(0,offset.top);
+}
 
       //localScroll
 
@@ -231,8 +178,10 @@ var sprig = {
       });
 
 
+
       $(document).ready(function()
       {
+
 
 
 
@@ -263,7 +212,9 @@ var sprig = {
 
       var pH = $(window).height();
 
-      $('.hero1').followTo(pH);
+      $('.hero1').followTo(pH+70);
+
+      $('.navbar-default').ResponsiveMenu(200);
 
 
 });
